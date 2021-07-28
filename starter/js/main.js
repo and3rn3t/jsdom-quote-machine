0a139cfee681faf72ec53617d2db512029c75e9e
Anderson Matthew J <andersonmatthewj@johndeere.com>
2021-07-28 11:03:23 -0500

    Finished steps 1-5

--- starter/js/main.js
+++ starter/js/main.js
@@ -1,11 +1,18 @@
-// console.log('main.js loaded');
-
+// defining variables to parts of the page I will need for input and listeners
+let heroQuote = document.querySelector('.hero blockquote');
+let heroAuthor = document.querySelector('.hero p');
+let quoteList = document.querySelector('.quotes');
 let quoteTxt = document.querySelector('#quote-txt');
-let authorTxt = document.querySelector('#author');
-let submitBtn = document.querySelector('#save-btn');
-let newQuote;
-let newAuthor;
+let author = document.querySelector('#author');
 
-submitBtn.addEventListener('click', event => {
-    console.log(quoteTxt.innerHTML);
+// grabbing Save button to use in event listener
+let saveBtn = document.querySelector('#save-btn');
+
+saveBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    let inputQuote = quoteTxt.value;
+    let inputAuthor = author.value;
+
+    heroQuote.innerHTML = inputQuote;
+    heroAuthor.innerHTML = inputAuthor;
 })
