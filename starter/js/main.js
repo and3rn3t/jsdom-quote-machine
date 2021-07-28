8fe83d091c978b93a68fce80d36b6e5158787436
Anderson Matthew J <andersonmatthewj@johndeere.com>
2021-07-28 09:50:10 -0500

    added variables for DOM and submit listener

--- starter/js/main.js
+++ starter/js/main.js
@@ -1,2 +1,11 @@
-console.log('main.js loaded');
+// console.log('main.js loaded');
 
+let quoteTxt = document.querySelector('#quote-txt');
+let authorTxt = document.querySelector('#author');
+let submitBtn = document.querySelector('#save-btn');
+let newQuote;
+let newAuthor;
+
+submitBtn.addEventListener('click', event => {
+    console.log(quoteTxt.innerHTML);
+})
