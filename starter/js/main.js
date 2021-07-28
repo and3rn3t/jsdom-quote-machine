b42e10fcbf17e705104fd0946505d7005182f0ec
Anderson Matthew J <andersonmatthewj@johndeere.com>
2021-07-28 11:46:15 -0500

    Added quote generator function

--- starter/js/main.js
+++ starter/js/main.js
@@ -8,6 +8,7 @@ let author = document.querySelector('#author');
 // grabbing Save button to use in event listener
 let saveBtn = document.querySelector('#save-btn');
 
+// Event listener to add input quotation to hero banner
 saveBtn.addEventListener('click', (event) => {
     event.preventDefault();
     let inputQuote = quoteTxt.value;
@@ -16,6 +17,18 @@ saveBtn.addEventListener('click', (event) => {
     heroQuote.innerHTML = inputQuote;
     heroAuthor.innerHTML = inputAuthor;
 
+    // Clears input text values
     quoteTxt.value = '';
     author.value = '';
-})
+});
+
+let quoteGenerator = (quoteTxt, author, quoteList) => {
+    let quote = document.createElement('li');
+    quote.setAttribute('class', 'quote');
+
+    let newQuoteTxt = document.createElement('blockquote');
+    newQuoteTxt.innerText = quoteTxt;
+
+    let newAuthor = document.createElement('p');
+    newAuthor.innerText = author;
+};
